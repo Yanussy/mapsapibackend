a23731c3174677103441dd9dafbc898d389d3617
yanussy <yanahello333@gmail.com>
2024-09-15 19:19:37 +0300

    gospodi pazi

--- config/db.js
+++ config/db.js
@@ -4,11 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true, // Make sure to use appropriate options
-      useFindAndModify: false,
-    });
+         });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,20 @@
 
+
+// controllers/userController.js
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-exports.loginOrSignUp = async (req, res) => {
-  const { email, password } = req.body;
-
+const getUserByEmail = async (req, res) => {
   try {
-    let user = await User.findOne({ email });
-    let isNewUser = false;
-
+    const { email } = req.params;
+    const user = await User.findOne({ email });
     if (!user) {
-      const hashedPassword = await bcrypt.hash(password, 12);
-      user = new User({ email, password: hashedPassword });
-      await user.save();
-      isNewUser = true;
-    } else {
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials' });
-      }
+      return res.status(404).json({ message: 'User not found' });
     }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    res.status(isNewUser ? 201 : 200).json({
-      token,
-      user: { email: user.email, id: user._id },
-    });
+    res.json(user);
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
+    res.status(500).json({ message: 'Server error' });
   }
 };
+
+module.exports = { getUserByEmail };
+
