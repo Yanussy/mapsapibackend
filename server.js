82042b4fbae35cb6b9bcefad250c71a3131974ac
yanussy <yanahello333@gmail.com>
2024-09-18 10:17:20 +0300

    pls girl

--- server.js
+++ server.js
@@ -5,22 +5,29 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const helmet = require('helmet');
 // Load environment variables
 require('dotenv').config();
 
 // Connect to database
 connectDB();
 
+app.use(helmet());
 
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline' https://unpkg.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://your-api-domain.com;"
-  );
-  next();
-});
+a
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://mapsapibackend.onrender.com"],
+      styleSrc: ["'self'", "https://mapsapibackend.onrender.com"],
+      imgSrc: ["'self'", "data:", "https://mapsapibackend.onrender.com"],
+      connectSrc: ["'self'", "https://mapsapibackend.onrender.com"],
+      fontSrc: ["'self'", "https://mapsapibackend.onrender.com"],
+    },
+  })
+);
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || '*' // Replace with your frontend's URL for security in production
